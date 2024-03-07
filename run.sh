45ea29b99f6f5e7db209c5c9eb83698a0d43d408
pengrui <362573456@qq.com>
2024-03-07 09:14:38 +0800

    bash

--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+SEP=:
+PATH_INFO=
+JAR_DIR=/d/software/openjfx/javafx-sdk-21.0.2/lib
+for file in `ls $JAR_DIR`
+do
+  PATH_INFO=$JAR_DIR/$file$SEP$PATH_INFO
+done
+PATH_INFO=$PATH_INFO/d/project/fx-hello/out/production/fx-hello
+echo $PATH_INFO
+java -classpath $PATH_INFO fx.Main
